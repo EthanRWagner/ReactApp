89e3dce172494def3db64a45bdf8130c7ed5eef3
OzzY <ewagne02@calpoly.edu>
2022-10-02 22:23:13 -0700

    Revert "post call"
    This reverts commit 4919c4afbb637142d24c0f25428d046a4b1521ae.

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -14,10 +14,7 @@ function MyApp() {
   }
   
   function updateList(person) {
-    makePostCall(person).then( result => {
-      if (result && result.status === 200)
-        setCharacters([...characters, person] );
-    });
+    setCharacters([...characters, person]);
   }
 
   // asynchronous function to get users from user list from backend.
@@ -44,17 +41,6 @@ function MyApp() {
         setCharacters(result);
     });
   }, [] );
-
-  async function makePostCall(person){
-    try {
-      const response = await axios.post('http://localhost:5000/users', person);
-      return response;
-    }
-    catch(error){
-      console.log(error);
-      return false;
-    }
-  }
   
   return (
     <div className="container">
